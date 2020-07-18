0f301786f8f7cf12f491efe976ea2e54c4041270
AshishJha-code <68464300+AshishJha-code@users.noreply.github.com>
2020-07-18 17:25:00 +0530

    Initial commit

---
+++ app/src/main/java/exportkit/xd/iphone_6_7_8_plus__1_activity.java
@@ -0,0 +1,46 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_6_7_8_plus__1
+	 *	@date 		0
+	 *	@title 		iPhone 6/7/8 Plus 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.xd
+	 *
+	 */
+	
+
+package exportkit.xd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+
+public class iphone_6_7_8_plus__1_activity extends Activity {
+
+	
+	private View _bg__iphone_6_7_8_plus__1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_6_7_8_plus__1);
+
+		
+		_bg__iphone_6_7_8_plus__1 = (View) findViewById(R.id._bg__iphone_6_7_8_plus__1);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
---
+++ app/src/main/java/exportkit/xd/iphone_6_7_8_plus__2_activity.java
@@ -0,0 +1,114 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		iphone_6_7_8_plus__1
+	 *	@date 		0
+	 *	@title 		iPhone 6/7/8 Plus 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.xd
+	 *
+	 */
+	
+
+package exportkit.xd;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class iphone_6_7_8_plus__2_activity extends Activity {
+
+	
+	private View rectangle_62;
+	private View ellipse_34;
+	private ImageView rectangle_63;
+	private ImageView path_132;
+	private ImageView path_133;
+	private ImageView path_134;
+	private ImageView path_135;
+	private ImageView path_136;
+	private ImageView path_137;
+	private ImageView path_138;
+	private ImageView path_139;
+	private ImageView path_140;
+	private ImageView path_141;
+	private ImageView path_142;
+	private ImageView path_143;
+	private ImageView path_144;
+	private ImageView path_145;
+	private ImageView path_146;
+	private TextView halestein_;
+	private TextView foodlabs;
+	private TextView login;
+	private TextView email_id_mobile_no_;
+	private TextView password;
+	private ImageView line_19;
+	private TextView or_login_with;
+	private TextView sign_up;
+	private TextView don_t_have_an_account_;
+	private ImageView rectangle_64;
+	private ImageView rectangle_65;
+	private ImageView line_20;
+	private ImageView line_21;
+	private ImageView line_22;
+	private ImageView rectangle_66;
+	private ImageView rectangle_67;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.iphone_6_7_8_plus__2);
+
+		
+		rectangle_62 = (View) findViewById(R.id.rectangle_62);
+		ellipse_34 = (View) findViewById(R.id.ellipse_34);
+		rectangle_63 = (ImageView) findViewById(R.id.rectangle_63);
+		path_132 = (ImageView) findViewById(R.id.path_132);
+		path_133 = (ImageView) findViewById(R.id.path_133);
+		path_134 = (ImageView) findViewById(R.id.path_134);
+		path_135 = (ImageView) findViewById(R.id.path_135);
+		path_136 = (ImageView) findViewById(R.id.path_136);
+		path_137 = (ImageView) findViewById(R.id.path_137);
+		path_138 = (ImageView) findViewById(R.id.path_138);
+		path_139 = (ImageView) findViewById(R.id.path_139);
+		path_140 = (ImageView) findViewById(R.id.path_140);
+		path_141 = (ImageView) findViewById(R.id.path_141);
+		path_142 = (ImageView) findViewById(R.id.path_142);
+		path_143 = (ImageView) findViewById(R.id.path_143);
+		path_144 = (ImageView) findViewById(R.id.path_144);
+		path_145 = (ImageView) findViewById(R.id.path_145);
+		path_146 = (ImageView) findViewById(R.id.path_146);
+		halestein_ = (TextView) findViewById(R.id.halestein_);
+		foodlabs = (TextView) findViewById(R.id.foodlabs);
+		login = (TextView) findViewById(R.id.login);
+		email_id_mobile_no_ = (TextView) findViewById(R.id.email_id_mobile_no_);
+		password = (TextView) findViewById(R.id.password);
+		line_19 = (ImageView) findViewById(R.id.line_19);
+		or_login_with = (TextView) findViewById(R.id.or_login_with);
+		sign_up = (TextView) findViewById(R.id.sign_up);
+		don_t_have_an_account_ = (TextView) findViewById(R.id.don_t_have_an_account_);
+		rectangle_64 = (ImageView) findViewById(R.id.rectangle_64);
+		rectangle_65 = (ImageView) findViewById(R.id.rectangle_65);
+		line_20 = (ImageView) findViewById(R.id.line_20);
+		line_21 = (ImageView) findViewById(R.id.line_21);
+		line_22 = (ImageView) findViewById(R.id.line_22);
+		rectangle_66 = (ImageView) findViewById(R.id.rectangle_66);
+		rectangle_67 = (ImageView) findViewById(R.id.rectangle_67);
+	
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
